30771b8cb1d4dc7cc2d2b69fcf5783e5d65fe003
HenryqueprogramaC <israelhenryque50000@gmail.com>
2025-03-27 18:12:04 +0000

    CartasSuperTrunfo.c projeto final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,59 +1,78 @@
 #include <stdio.h>
 
-
-
-int main() {
-    char codestado;
-    char nome[50];
-    float populacao;
-    double area;
+int main(){
+    char codecity[20];
+    int populacao;
+    int area;
     int pib;
-    float pt;
-
-    printf("Diga o codigo do seu estado:  \n");
-    scanf( "%c", &codestado);
-    getchar();
-
-    printf("Diga o nome da sua ciade:  \n");
-    scanf("%s", nome);
-    getchar();
-
-    printf("Diga a população da sua cidade:  \n");
-    scanf( "%f", &populacao);
-    getchar();
-
-    printf("Diga a area da sua cidade:  \n");
-    scanf("%lf", &area);
-    getchar();
-
-    printf("Diga o pib da sua cidade:  \n");
+    int numpt;
+   
+ 
+    char codecity2[20];
+    int populacao2;
+    int area2;
+    int pib2;
+    int numpt2;
+ 
+    printf("Diga o codigo da sua cidade :\n");
+    scanf("%19s", codecity);
+    
+ 
+    printf("Diga a população da sua cidade :\n");
+    scanf("%d", &populacao);
+   
+ 
+    printf("Diga a area da sua cidade :\n");
+    scanf("%d", &area);
+    
+ 
+    printf("Diga o pib da sua cidade :\n");
     scanf("%d", &pib);
-
-    getchar();
-
-    printf("Diga a quantidades de pontos turisticos da sua ciadede:  \n");
-    scanf("%f", &pt);
-
-    getchar();
-
-    printf("Otimo!!, sua cidade tem: Nome: %s População: %f Area: %lf PIB: %d Pontos turisticos: %f", codestado, nome, populacao, area, pib, pt);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
+ 
+    printf("Diga o numero de pontos turisticos da sua cidade :\n");
+    scanf("%d", &numpt);
+ 
+    int densidade = populacao / area;
+    int pibpercapita = pib / populacao;
+    
+    printf("Densidade populacional: %d\n", densidade);
+    
+    printf("Pib per capita :%d\n", pibpercapita);
+ 
+ 
+    printf("Diga o codigo da sua cidade :\n");
+    scanf("%19s", codecity2);
+    
+ 
+    printf("Diga a população da sua cidade :\n");
+    scanf("%d", &populacao2);
+   
+ 
+    printf("Diga a area da sua cidade :\n");
+    scanf("%d", &area2);
+    
+ 
+    printf("Diga o pib da sua cidade :\n");
+    scanf("%d", &pib2);
+   
+ 
+    printf("Diga o numero de pontos turisticos da sua cidade :\n");
+    scanf("%d", &numpt2);
+     
+    int densidade2 = populacao2 / area2;
+    int pibpercapita2 = pib2 / populacao2;
+ 
+    printf("Densidade populacional: %d\n", densidade2);
+    
+    printf("Pib per capita: %d\n", pibpercapita2);
+    
+    printf("População da 1° cidade é maior que a da segunda?: %d\n", populacao > populacao2);
+    printf("Area da 1° cidade é maior que a da segunda?: %d\n", area > area2);
+    printf("Pib da 1° cidade é maior que a da segunda?: %d\n", pib > pib2);
+    printf("Número de pontos turisticos da 1° cidade é maior que a da segunda?: %d\n", numpt > numpt2);
+    printf("Densidade populacional da 1° cidade é maior que a da segunda?: %d\n", densidade < densidade2);
+    printf("Pib per capita da 1° cidade é maior que a da segunda?: %d\n", pibpercapita > pibpercapita2);
+    
     return 0;
-}
+ }
